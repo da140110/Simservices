084a7e16cbdc760f1ca03581104359a6b52f0894
daotuanlinh <daotuanlinh@gmail.com>
2019-02-21 09:47:23 +0700

    Model Done

---
+++ SimServices.Data/SimServicesDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimServices.Model;
+using Si
+
+namespace SimServices.Data
+{
+    public class SimServicesDbContext : DbContext
+    {
+        public SimServicesDbContext() : base ("SimServicesConnection")
+        {
+            Configuration.LazyLoadingEnabled = false;
+        }
+        public DbSet <Footer>
+    }
+}
--- SimServies.Web/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SimServies.Web.MvcApplication" Language="C#" %>
